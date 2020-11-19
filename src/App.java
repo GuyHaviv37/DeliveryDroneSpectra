98641e35c7878c13de91a86db8233faedfa6d89c
Guy Haviv <theguyhaviv@gmail.com>
2020-11-19 12:02:57 +0200

    update to simulation

--- src/App.java
+++ src/App.java
@@ -28,107 +28,23 @@ public class App extends JComponent {
 
 	ControllerExecutor executor;
 	//env
+	Map<String,String> inputs;
 	boolean[] outHousePackages = new boolean[NUM_OF_HOUSES];
 	boolean[] outWarehousePackages= new boolean[NUM_OF_HOUSES];
 	//sys
+	Map<String, String> sysValues;
 	int[] drone= new int[2];
 	int[] droneToHouseCap= new int[NUM_OF_HOUSES];
 	int droneToWarehouseCap;
 	boolean[] pickUpThisStateFromWareHouse= new boolean[NUM_OF_HOUSES];
 	boolean[] pickUpThisStateFromHouse= new boolean[NUM_OF_HOUSES];
 	int totalPackages=0;
-	boolean waitingHouse0;
 	// simulation variables
 	BufferedImage droneImg;
 	int squareSize = 145;
 	int droneSize = 125;
 
 	Thread thread;
-	void initAllArraysToFalse() {
-		for(int i=0;i<NUM_OF_HOUSES;i++) {
-			outHousePackages[i]=false;
-			outWarehousePackages[i]=false;
-		}
-	}
-	void getEnvValueFromConsole()  {
-		try {
-			BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
-			System.out.println("enter house # which get new out package (# between 0-3 include): example- 2 3");
-			String outHousePackages_console = reader.readLine(); 
-			System.out.println("enter house # which get new package in warehouse (# between 0-3 include): example- 1 0");
-			String warehousePackages_console = reader.readLine();  
-
-			String[] split_houses = outHousePackages_console.split(" ");
-			String[] split_warehouse = warehousePackages_console.split(" ");
-			initAllArraysToFalse();
-			for(int i=0; i<split_houses.length;i++) {
-				outHousePackages[Integer.parseInt(split_houses[i])]=true;
-			}
-			for(int i=0; i<split_warehouse.length;i++) {
-				outWarehousePackages[Integer.parseInt(split_warehouse[i])]=true;
-			}		
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
-	void updateInputsWithCurrentVal(Map<String,String> inputs) {
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			String outHouse = "outHousePackages[" + Integer.toString(i)+"]";
-			String outWarehouse = "outWarehousePackages[" + Integer.toString(i)+"]";
-			inputs.put(outHouse, Boolean.toString(outHousePackages[i]) );
-			inputs.put(outWarehouse, Boolean.toString(outWarehousePackages[i]));
-		}
-	}
-	void updateSysVaribleValue(Map<String, String> sysValues) {
-
-		drone[0] = Integer.parseInt(sysValues.get("drone[0]"));
-		drone[1] = Integer.parseInt(sysValues.get("drone[1]"));
-		droneToWarehouseCap = Integer.parseInt(sysValues.get("droneToWarehouseCap"));
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			droneToHouseCap[i] = Integer.parseInt(sysValues.get("droneToHouseCap["+Integer.toString(i)+"]"));
-			pickUpThisStateFromWareHouse[i] = Boolean.parseBoolean(sysValues.get("pickUpThisStateFromWareHouse["+Integer.toString(i)+"]"));
-			pickUpThisStateFromHouse[i] = Boolean.parseBoolean(sysValues.get("pickUpThisStateFromHouse["+Integer.toString(i)+"]"));
-		}	
-		totalPackages =Integer.parseInt(sysValues.get("totalPackages"));
-		waitingHouse0 =Boolean.parseBoolean(sysValues.get("waitingPackageOutHouse0"));
-	}
-	void printEnvValue() {
-		System.out.println("Envirenment varibales");
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			System.out.print("  |  outHousePackages["+Integer.toString(i)+"]= ");
-			System.out.print(outHousePackages[i]);
-		}
-		System.out.println();
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			System.out.print("  |  outWarehousePackages["+Integer.toString(i)+"]= ");
-			System.out.print(outWarehousePackages[i]);
-		}
-		System.out.println();
-	}
-	void printSysValue() {
-		System.out.println("system varibales");
-		System.out.println("totalPackages= " + totalPackages);
-		System.out.println("drone[0] = " + drone[0] +"  |  drone[1] = " +drone[1]);
-		System.out.println("droneToWarehouseCap = " + droneToWarehouseCap);
-		System.out.println("waitingPackageOutHouse0 = " + waitingHouse0);
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			System.out.print("  |  droneToHouseCap["+Integer.toString(i)+"]= ");
-			System.out.print(droneToHouseCap[i]);
-		}
-		System.out.println();
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			System.out.print("  |  pickUpThisStateFromWareHouse["+Integer.toString(i)+"]= ");
-			System.out.print(pickUpThisStateFromWareHouse[i]);
-		}
-		System.out.println();
-		for(int i=0; i<NUM_OF_HOUSES;i++) {
-			System.out.print("  |  pickUpThisStateFromHouse["+Integer.toString(i)+"]= ");
-			System.out.print(pickUpThisStateFromHouse[i]);
-		}	
-		System.out.println();
-	}
 	
 	public App() {
 		Thread animationThread = new Thread(new Runnable() {
@@ -140,11 +56,11 @@ public class App extends JComponent {
 					e.printStackTrace();
 					System.exit(0);
 				}
-				Map<String,String> inputs = new HashMap<String, String>();
-				Map<String, String> sysValues;
+				inputs = new HashMap<String, String>();
+				
 				initAllArraysToFalse(); // keep asm on the env
 				updateInputsWithCurrentVal(inputs);
-				printEnvValue();
+				//printEnvValue();
 				executor.initState(inputs);
 				repaint();
 
@@ -152,13 +68,13 @@ public class App extends JComponent {
 
 					sysValues = executor.getCurrOutputs();
 					updateSysVaribleValue(sysValues);
-					printSysValue();
+					//printSysValue();
 					
 					repaint();
 
 					getEnvValueFromConsole();
 					updateInputsWithCurrentVal(inputs);
-					printEnvValue();
+					//printEnvValue();
 					executor.updateState(inputs);
 				}
 			}
@@ -189,6 +105,16 @@ public class App extends JComponent {
 				g.fillRect(col*squareSize, row*squareSize, squareSize, squareSize);
 				g.setColor(Color.black);
 				g.drawString("House"+(houseNum), col*squareSize + 20, row*squareSize + 20);
+				if(sysValues != null) {
+					if(Boolean.parseBoolean(sysValues.get("waitingPackageOutHouse"+houseNum))){
+						g.setColor(Color.red);
+						g.drawString("Waiting!", col*squareSize + 20, (row+1)*squareSize - 40);
+					}
+					if(pickUpThisStateFromHouse[houseNum]) {
+						g.setColor(Color.green);
+						g.drawString("Picked Up!", col*squareSize + 20, (row+1)*squareSize - 20);
+					}					
+				}
 				houseNum++;
 			}
 		}
@@ -208,10 +134,116 @@ public class App extends JComponent {
 		g.fillRect(col*squareSize, row*squareSize, squareSize, squareSize);
 		g.setColor(Color.black);
 		g.drawString("Warehouse", col*squareSize + 20, row*squareSize + 20);
+		// paint warehouse details
+		row = 3;
+		col = 2;
+		if(sysValues!=null) {
+			g.drawString("WH Details",col*squareSize + 20, row*squareSize + 20);
+			for(houseNum=0;houseNum<4;houseNum++) { 
+				String waiting = Boolean.parseBoolean(sysValues.get("waitingPackageInWarehouseToHouse"+houseNum)) ? "W" : "";
+				String pickedUp = pickUpThisStateFromWareHouse[houseNum] ? "PU" : "";
+				g.drawString("H"+houseNum+": "+waiting+" "+pickedUp, col*squareSize + 20, row*squareSize + 20*(houseNum+2));
+			}			
+		}
 		
+		//paint drone details
+		row = 3;
+		col = 0;
+		g.drawString("Drone Details", col*squareSize + 20, row*squareSize + 20);
+		g.drawString("Total: "+totalPackages, col*squareSize + 20, row*squareSize + 40);
+		g.drawString("To-WH: "+droneToWarehouseCap, col*squareSize + 20, row*squareSize + 60);
+		for(houseNum=0;houseNum<4;houseNum++) { 
+			g.drawString("To-H"+houseNum+": "+droneToHouseCap[houseNum], col*squareSize + 20, row*squareSize + 20*(houseNum+4));
+		}	
+
 		// "paint drone"
 		g.drawImage(droneImg, this.drone[1]*squareSize, this.drone[0]*squareSize, droneSize, droneSize, null);
 		
+	}
+	
+	void initAllArraysToFalse() {
+		for(int i=0;i<NUM_OF_HOUSES;i++) {
+			outHousePackages[i]=false;
+			outWarehousePackages[i]=false;
+		}
+	}
+	void getEnvValueFromConsole()  {
+		try {
+			BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in)); 
+			System.out.println("Enter House# for outgoing package request (# between 0-3 include): example- 2 3");
+			String outHousePackages_console = reader.readLine(); 
+			System.out.println("Enter House# for warehouse package request (# between 0-3 include): example- 1 0");
+			String warehousePackages_console = reader.readLine();  
+
+			String[] split_houses = outHousePackages_console.split(" ");
+			String[] split_warehouse = warehousePackages_console.split(" ");
+			initAllArraysToFalse();
+			for(int i=0; i<split_houses.length;i++) {
+				outHousePackages[Integer.parseInt(split_houses[i])]=true;
+			}
+			for(int i=0; i<split_warehouse.length;i++) {
+				outWarehousePackages[Integer.parseInt(split_warehouse[i])]=true;
+			}
+			System.out.println("-----------------------------------------------------");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	void updateInputsWithCurrentVal(Map<String,String> inputs) {
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			String outHouse = "outHousePackages[" + Integer.toString(i)+"]";
+			String outWarehouse = "outWarehousePackages[" + Integer.toString(i)+"]";
+			inputs.put(outHouse, Boolean.toString(outHousePackages[i]) );
+			inputs.put(outWarehouse, Boolean.toString(outWarehousePackages[i]));
+		}
+	}
+	void updateSysVaribleValue(Map<String, String> sysValues) {
+
+		drone[0] = Integer.parseInt(sysValues.get("drone[0]"));
+		drone[1] = Integer.parseInt(sysValues.get("drone[1]"));
+		droneToWarehouseCap = Integer.parseInt(sysValues.get("droneToWarehouseCap"));
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			droneToHouseCap[i] = Integer.parseInt(sysValues.get("droneToHouseCap["+Integer.toString(i)+"]"));
+			pickUpThisStateFromWareHouse[i] = Boolean.parseBoolean(sysValues.get("pickUpThisStateFromWareHouse["+Integer.toString(i)+"]"));
+			pickUpThisStateFromHouse[i] = Boolean.parseBoolean(sysValues.get("pickUpThisStateFromHouse["+Integer.toString(i)+"]"));
+		}	
+		totalPackages =Integer.parseInt(sysValues.get("totalPackages"));
+	}
+	void printEnvValue() {
+		System.out.println("Envirenment varibales");
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			System.out.print("  |  outHousePackages["+Integer.toString(i)+"]= ");
+			System.out.print(outHousePackages[i]);
+		}
+		System.out.println();
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			System.out.print("  |  outWarehousePackages["+Integer.toString(i)+"]= ");
+			System.out.print(outWarehousePackages[i]);
+		}
+		System.out.println();
+	}
+	void printSysValue() {
+		System.out.println("system varibales");
+		System.out.println("totalPackages= " + totalPackages);
+		System.out.println("drone[0] = " + drone[0] +"  |  drone[1] = " +drone[1]);
+		System.out.println("droneToWarehouseCap = " + droneToWarehouseCap);
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			System.out.print("  |  droneToHouseCap["+Integer.toString(i)+"]= ");
+			System.out.print(droneToHouseCap[i]);
+		}
+		System.out.println();
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			System.out.print("  |  pickUpThisStateFromWareHouse["+Integer.toString(i)+"]= ");
+			System.out.print(pickUpThisStateFromWareHouse[i]);
+		}
+		System.out.println();
+		for(int i=0; i<NUM_OF_HOUSES;i++) {
+			System.out.print("  |  pickUpThisStateFromHouse["+Integer.toString(i)+"]= ");
+			System.out.print(pickUpThisStateFromHouse[i]);
+		}	
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
