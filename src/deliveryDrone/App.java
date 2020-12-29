f24f9986f3b1bf0d9adbc2ba9f40545dc90ef059
noadav15 <noadav96@gmail.com>
2020-12-29 17:19:10 +0200

    clean main

--- src/deliveryDrone/App.java
+++ src/deliveryDrone/App.java
@@ -1,66 +1,13 @@
 package deliveryDrone;
 import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 public class App {
 
 	public static void main(String[] args) {
-		boolean priority = false ;
-		boolean winds = false; 
-		boolean energy = false; 
-		JFrame frame = new JFrame("Config");
-		final String[] options = { "Default - All Features Enabled","Priority and Winds Mode Only",
-				"Priority Mode and Energy Only","Winds Mode and Energy Only",
-				"Priority Mode Only", "Winds Mode Only",
-				"Energy Only","No Extra Features"};
-		String selected = (String) JOptionPane.showInputDialog(frame, 
-				"Which Features Does Your Specification Use?",
-				"Config",
-				JOptionPane.QUESTION_MESSAGE, 
-				null, 
-				options, 
-				options[0]);
-		if(selected!= null) {
-			switch (selected) {
-			case "Default - All Features Enabled" :
-				priority=true;
-				winds=true;
-				energy=true;
-				break;
-			case "Priority and Winds Mode Only":
-				priority=true;
-				winds=true;
-				break;
-			case "Priority Mode and Energy Only":
-				priority=true;
-				energy=true;
-				break;
-			case "Winds Mode and Energy Only":
-				winds=true;
-				energy=true;
-				break;
-			case "Priority Mode Only":
-				priority=true;
-				break;
-			case "Winds Mode Only":
-				winds=true;
-				break;
-			case "Energy Only":
-				energy=true;
-				break;
-			case "No Extra Features":
-				break;
-			}
-		}
-		else {
-			priority=true;
-			winds=true;
-			energy=true;
-		}
-		final boolean priorityFeature = priority; 
-		final boolean windsFeature = winds; 
-		final boolean energyFeature = energy; 
+		boolean[] features = ModulizeFeature.getFeature();
+		final boolean priorityFeature = features[0]; 
+		final boolean windsFeature = features[1]; 
+		final boolean energyFeature = features[2]; 
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new MainFrame("DeliveryDrone",priorityFeature,windsFeature,energyFeature);
